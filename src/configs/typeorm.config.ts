15871a7d161d62e02419f2a7a58dd03a38c8cb34
Sekhudin <sekhudinuap@gmail.com>
2025-01-28 22:31:21 +0700

    setup database migrations

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,25 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as env from "./env.config";
+
+const options: Pick<DataSourceOptions, "entities" | "migrations"> = {
+  entities: env.isProduction()
+    ? ["../modules/user/entities/*.entity.js"]
+    : ["../modules/user/entities/*.entity.ts"],
+  migrations: env.isProduction() ? ["../../migrations/*js"] : ["../../migrations/*.ts"],
+};
+
+const dataSourceOptions: DataSourceOptions = {
+  type: env.DB_TYPE as "postgres",
+  database: env.DB_NAME,
+  schema: env.DB_SCHEMA,
+  host: env.DB_HOST,
+  port: Number(env.DB_PORT),
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  synchronize: false,
+  logging: true,
+  migrationsTableName: env.DB_MIGRATION_TABLE,
+  ...options,
+};
+
+export const dataSource = new DataSource(dataSourceOptions);
