ecac3cf31d49f194be14631b7a7c3267dc17715f
Sekhudin <sekhudinuap@gmail.com>
2025-02-03 15:18:59 +0700

    config: cookie

---
+++ src/configs/cookie.config.ts
@@ -0,0 +1,16 @@
+import type { CookieConfig } from "src/types/cookie.type";
+import * as env from "./env.config";
+
+const getMaxAgeInDay = (day: number) => {
+  return day * 24 * 60 * 60 * 1000;
+};
+
+export const jwtRefreshCookieConfig: CookieConfig<"name"> = {
+  name: env.JWT_REFRESH_COOKIE,
+  options: {
+    httpOnly: true,
+    secure: env.isMatch(env.JWT_REFRESH_COOKIE_SECURE, "true"),
+    sameSite: "strict",
+    maxAge: getMaxAgeInDay(6),
+  },
+};
