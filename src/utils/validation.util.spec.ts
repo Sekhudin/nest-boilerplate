f298816c20c007fd242174ee63c362aa1b29a46f
Sekhudin <sekhudinuap@gmail.com>
2025-02-20 15:20:49 +0700

    test(validation.util): add test script to test validation util

---
+++ src/utils/validation.util.spec.ts
@@ -0,0 +1,49 @@
+import z from "zod";
+import type { StandardSchemaV1 } from "@standard-schema/spec";
+import * as validation from "./validation.util";
+
+describe("validationUtil", () => {
+  type Person = {
+    name?: string;
+    age?: number;
+  };
+
+  let numberSchema: StandardSchemaV1<number, number>;
+  let personSchema: StandardSchemaV1<Person, Person>;
+
+  beforeEach(() => {
+    numberSchema = validation.createSchema(z.number());
+    personSchema = validation.createSchema(
+      z.object({
+        name: z.string().min(1),
+        age: z.number().min(1),
+      }),
+    );
+  });
+
+  describe("createSchema", () => {
+    it("tobeDefined", () => {
+      expect(numberSchema).toBeDefined();
+      expect(numberSchema["~standard"]).toBeDefined();
+      expect(numberSchema["~standard"].vendor).toBeDefined();
+      expect(numberSchema["~standard"].version).toBeDefined();
+      expect(numberSchema["~standard"].validate).toBeDefined();
+      expect(personSchema).toBeDefined();
+      expect(personSchema["~standard"]).toBeDefined();
+      expect(personSchema["~standard"].vendor).toBeDefined();
+      expect(personSchema["~standard"].version).toBeDefined();
+      expect(personSchema["~standard"].validate).toBeDefined();
+    });
+
+    it("toBeTruthy - validation success", () => {
+      expect(numberSchema["~standard"].validate(1)).toBeTruthy();
+      expect(personSchema["~standard"].validate({ name: "john doe", age: 10 })).toBeTruthy();
+    });
+
+    it("should be throw validation error", async () => {
+      expect(() => numberSchema["~standard"].validate("1")).toThrow(z.ZodError);
+      expect(() => personSchema["~standard"].validate({ name: "", age: 10 })).toThrow(z.ZodError);
+      expect(() => personSchema["~standard"].validate({ name: "john", age: 0 })).toThrow(z.ZodError);
+    });
+  });
+});
