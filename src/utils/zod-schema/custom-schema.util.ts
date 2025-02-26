fa917488312780d54731b889d9f713b29ace550a
Sekhudin <sekhudinuap@gmail.com>
2025-02-26 11:31:32 +0700

    add custom schema, that provide standar for special value like username, password, etc

---
+++ src/utils/zod-schema/custom-schema.util.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const requiredString = (field: string) => z.string().trim().min(1, `${field} can't be empty!`);
+export const email = () => z.string().trim().toLowerCase().email();
+
+export const username = () => requiredString("username").toLowerCase();
+export const password = () => requiredString("password");
