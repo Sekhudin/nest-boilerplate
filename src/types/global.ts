50d1df016d315258c0aed714722ad99fcb9d358e
Sekhudin <sekhudinuap@gmail.com>
2025-06-08 02:45:44 +0700

    refactor(types): remove unused global and testing types
    - Deleted  as it is no longer needed
    - Cleaned up unused types from  to reduce clutter

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,8 +1,13 @@
+import { Request, Response } from "express";
 import type { ApiOperationOptions } from "@nestjs/swagger";
 import type { StandardSchemaV1 } from "@standard-schema/spec";
 
 export type Docs<T extends string> = Record<T, ApiOperationOptions>;
 export type Dto<Schema extends StandardSchemaV1> = StandardSchemaV1.InferInput<Schema>;
+
 export type Nullable<T extends unknown> = T | null;
 
-export type Role = "user" | "admin";
+export interface RequestWithRes extends Request {
+  res: Response;
+}
+export type ExpressMiddleware = (req: Request, res: Response, next: () => void) => void;
--- src/types/testing.ts
+++
@@ -1,5 +0,0 @@
-import type { applyDecorators, createParamDecorator } from "@nestjs/common";
-
-export type MockRequest = Record<string, any>;
-export type ParamsDecorator = ReturnType<typeof createParamDecorator>;
-export type NonParamsDecorator = (...params: any[]) => ReturnType<typeof applyDecorators>;
