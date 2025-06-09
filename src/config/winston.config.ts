ef0126c5ac571e0990c6519d2b771af17f8defd7
Sekhudin <sekhudinuap@gmail.com>
2025-06-09 20:01:16 +0700

    chore(logging): update Winston config with custom transports and levels

--- src/config/winston.config.ts
+++ src/config/winston.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
-import { JsonLogFormatter } from "src/shared/classes/JsonLogFormatter";
-import { format, LoggerOptions, transports } from "winston";
+import safeStringify from "fast-safe-stringify";
+import { config, format, LoggerOptions, transports } from "winston";
 import { consoleFormat } from "winston-console-format";
 import DailyRotateFile from "winston-daily-rotate-file";
 import { BaseConfig } from "./base.config";
@@ -9,27 +9,26 @@ class WinstonConfig extends BaseConfig {
   private readonly _transportConsole: transports.ConsoleTransportInstance;
   private readonly _transportFile: DailyRotateFile;
   private readonly _transportHttp: DailyRotateFile;
+  private readonly _sensitiveData: Set<string>;
 
   constructor() {
     super();
+
+    this._sensitiveData = new Set([
+      "password",
+      "token",
+      "accessToken",
+      "refreshToken",
+      "email",
+      "phone",
+      "cvv",
+      "sex",
+      "age",
+    ]);
+
     this._transportConsole = new transports.Console({
-      format: format.combine(
-        format.colorize({ level: true }),
-        format.padLevels(),
-        consoleFormat({
-          showMeta: true,
-          metaStrip: ["timestamp", "service", "_app", "_version", "_env"],
-          inspectOptions: {
-            depth: Infinity,
-            colors: true,
-            maxArrayLength: Infinity,
-            breakLength: 80,
-            compact: Infinity,
-            sorted: true,
-            numericSeparator: true,
-          },
-        }),
-      ),
+      level: this.env.LOG_LEVEL_GLOBAL,
+      format: this.consoleFormat,
     });
 
     this._transportFile = new DailyRotateFile({
@@ -56,8 +55,9 @@ class WinstonConfig extends BaseConfig {
   }
 
   get options(): LoggerOptions {
+    config.addColors(this.colors);
     return {
-      level: "silly",
+      levels: this.levels,
       transports: [this._transportConsole, this._transportFile, this._transportHttp],
       exitOnError: false,
       defaultMeta: { _app: this.env.APP_NAME, _version: this.env.APP_VERSION, _env: this.env.APP_ENV },
@@ -70,15 +70,10 @@ class WinstonConfig extends BaseConfig {
     };
   }
 
-  get format() {
+  private get format() {
     switch (this.env.LOG_FORMAT) {
       case "json":
-        return format.combine(
-          format((value) => {
-            return JsonLogFormatter.transform(value);
-          })(),
-          format.json(),
-        );
+        return format.combine(this.formatMaskSensitive()(), format.json());
 
       case "pretty":
         return format.combine(
@@ -93,6 +88,62 @@ class WinstonConfig extends BaseConfig {
       default:
         return format.combine(format.timestamp(), format.simple());
     }
+  }
+
+  private get consoleFormat() {
+    return format.combine(
+      format.colorize({ level: true }),
+      format.padLevels(),
+      consoleFormat({
+        showMeta: true,
+        metaStrip: ["timestamp", "service", "_app", "_version", "_env"],
+        inspectOptions: {
+          depth: Infinity,
+          colors: true,
+          maxArrayLength: Infinity,
+          breakLength: 80,
+          compact: Infinity,
+          sorted: true,
+          numericSeparator: true,
+        },
+      }),
+    );
+  }
+
+  private get levels() {
+    return {
+      error: 0,
+      warn: 1,
+      http: 2,
+      info: 3,
+      verbose: 4,
+      debug: 5,
+      silly: 6,
+    };
+  }
+
+  private get colors() {
+    return {
+      error: "red",
+      warn: "yellow",
+      http: "magenta",
+      info: "green",
+      verbose: "cyan",
+      debug: "blue",
+      silly: "grey",
+    };
+  }
+
+  private formatMaskSensitive() {
+    return format((transformValue) => {
+      const safeValue = safeStringify(transformValue, (key, value) => {
+        if (this._sensitiveData.has(key)) {
+          return "*****";
+        }
+        return value;
+      });
+      return JSON.parse(safeValue) as any;
+    });
   }
 }
 
