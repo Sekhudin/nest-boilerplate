87c0685e120ba929981e06d0241b05ecd8b4b801
Sekhudin <sekhudinuap@gmail.com>
2025-06-16 00:22:49 +0700

    chore(app): setup global modules and clean bootstrap logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
-import { LoggerModule } from "./shared/modules/logger.module";
+import { ContextMiddleware } from "./shared/middlewares/context.middleware";
+import { ContextModule } from "./shared/modules/global/context/context.module";
+import { LoggerModule } from "./shared/modules/global/logger/logger.module";
 
 @Module({
-  imports: [LoggerModule, AuthModule],
+  imports: [ContextModule, LoggerModule, AuthModule],
   providers: [],
   exports: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ContextMiddleware).forRoutes("*path");
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "src/app.module";
 import { AllExceptionFilter } from "src/shared/filters/all-exception.filter";
 import { HttpInterceptor } from "src/shared/interceptors/http.interceptor";
 import { SerializerInterceptor } from "src/shared/interceptors/serializer.interceptor";
-import { LoggerService } from "src/shared/services/logger.service";
+import { LoggerService } from "src/shared/modules/global/logger/logger.service";
 import { appConfig } from "./config/app.config";
 import { cookieConfig } from "./config/cookie.config";
 import { corsConfig } from "./config/cors.config";
@@ -12,14 +12,14 @@ import { swaggerConfig } from "./config/swagger.config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const httpAdapterHost = app.get(HttpAdapterHost);
+  const reflector = app.get(Reflector);
   const logger = app.get(LoggerService);
 
-  appConfig.setup(app);
   corsConfig.setup(app);
   cookieConfig.setup(app);
   swaggerConfig.setup(app);
 
-  app.useGlobalInterceptors(new SerializerInterceptor(app.get(Reflector)), new HttpInterceptor(logger));
+  app.useGlobalInterceptors(new SerializerInterceptor(reflector), new HttpInterceptor(logger));
   app.useGlobalFilters(new AllExceptionFilter(httpAdapterHost, logger));
 
   await app.listen(appConfig.port, () => {
