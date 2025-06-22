9ece3c9f4567a30d63e192d6527034c4beb8498c
Sekhudin <sekhudinuap@gmail.com>
2025-06-22 22:20:58 +0700

    feat(app): integrate MailerModule and CryptoModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
+import { TokenModule } from "./modules/token/token.module";
 import { UserModule } from "./modules/user/user.module";
 import { ContextMiddleware } from "./shared/middlewares/context.middleware";
 import { ContextModule } from "./shared/modules/global/context/context.module";
 import { LoggerModule } from "./shared/modules/global/logger/logger.module";
+import { MailerModule } from "./shared/modules/global/mailer/mailer.module";
 
 @Module({
-  imports: [ContextModule, LoggerModule, AuthModule, UserModule],
+  imports: [ContextModule, LoggerModule, MailerModule, AuthModule, UserModule, TokenModule],
   providers: [],
   exports: [],
 })
