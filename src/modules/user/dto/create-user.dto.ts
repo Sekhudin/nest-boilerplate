dc30b4ff1bea71befb89e3d38a14096763a57b0c
Sekhudin <sekhudinuap@gmail.com>
2025-06-29 18:48:13 +0700

    refactor(auth): expose zod dto via static dto in user entities

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
+import { schema, Schema, z, zr } from "src/utils/validation";
+
+const createUser = z.object({});
+
+createUser.partial().pick({});
+
 export class CreateUserDto {}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -11,6 +11,7 @@ import { AuthHistory } from "src/modules/auth/entities/auth-history.entity";
 import { UserAuth } from "src/modules/auth/entities/user-auth.entity";
 import { Role } from "src/modules/role/entities/role.entity";
 import { Token } from "src/modules/token/entities/token.entity";
+import { z, zr } from "src/utils/validation";
 import { databaseConfig } from "src/config/database.config";
 
 @Entity(databaseConfig.table.user)
@@ -26,6 +27,15 @@ export class User {
 
   @Column({ default: true })
   isActive: boolean;
+
+  @Column({ default: false })
+  isEmailVerified: boolean;
+
+  @Column({ nullable: true, length: 128 })
+  emailVerificationToken: string;
+
+  @Column({ type: "timestamp", nullable: true })
+  emailVerificationTokenExpiresAt: Date;
 
   @ManyToOne(() => Role)
   role: Role;
@@ -44,4 +54,30 @@ export class User {
 
   @UpdateDateColumn()
   updatedAt: Date;
+
+  static get dto() {
+    return z.object({
+      id: z.uuidv4(),
+      email: z.email(),
+      password: zr.password(),
+      isActive: z.boolean(),
+      isEmailVerified: z.boolean(),
+      emailVerificationToken: z.string().length(6),
+      emailVerificationTokenExpiresAt: z.date(),
+      get role() {
+        return Role.dto;
+      },
+      get authMethods() {
+        return z.array(UserAuth.dto);
+      },
+      get authHistories() {
+        return z.array(AuthHistory.dto);
+      },
+      get tokens() {
+        return z.array(Token.dto);
+      },
+      timestamp: z.date(),
+      updatedAt: z.date(),
+    });
+  }
 }
