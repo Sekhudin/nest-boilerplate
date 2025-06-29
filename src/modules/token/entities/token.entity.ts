b6529700ff4e5976a4f74d6afa0d41af585d7447
Sekhudin <sekhudinuap@gmail.com>
2025-06-29 18:47:59 +0700

    refactor(auth): expose zod dto via static dto in token entities

--- src/modules/token/entities/token.entity.ts
+++ src/modules/token/entities/token.entity.ts
@@ -1,5 +1,6 @@
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "src/modules/user/entities/user.entity";
+import { z } from "src/utils/validation";
 import { databaseConfig } from "src/config/database.config";
 
 @Entity(databaseConfig.table.token)
@@ -24,4 +25,18 @@ export class Token {
 
   @CreateDateColumn()
   createdAt: Date;
+
+  static get dto() {
+    return z.object({
+      id: z.uuidv4(),
+      token: z.string().max(512),
+      ipAddress: z.string(),
+      userAgentString: z.string(),
+      revoked: z.boolean(),
+      get user() {
+        return User.dto;
+      },
+      createdAt: z.date(),
+    });
+  }
 }
