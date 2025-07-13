d4fef1ae643fd8742ca7cadc8deea46682bacc30
Sekhudin <sekhudinuap@gmail.com>
2025-07-14 03:27:34 +0700

    test(mocks): update AuthService mock for unit testing needs

--- test/mocks/services/auth.service.mock.ts
+++ test/mocks/services/auth.service.mock.ts
@@ -3,5 +3,7 @@ import { AuthService } from "src/modules/auth/auth.service";
 
 export const getFreshAuthServiceMock = () => {
   const service = mock<AuthService>();
+
+  service.signUpLocal.mockResolvedValue({ code: "123456", expiresAt: new Date(), expiresInMinutes: 5 });
   return service;
 };
