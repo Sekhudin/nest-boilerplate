5b952fdb67414178c5c4362de7964ff454e74785
Sekhudin <sekhudinuap@gmail.com>
2025-07-23 10:29:29 +0700

    refactor: grouping dto into requests and responses
    feat: add markIsEmailVerified method to change isEmailVerified properti to true

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,9 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserEmailAlreadyUsedException } from "src/shared/exceptions/user/user-email-already-used.exception";
-import { RoleService } from "src/modules/role/role.service";
+import { getFreshRoleMock } from "test/mocks/entities/role.entity.mock";
 import { getFreshUserMock } from "test/mocks/entities/user.entity.mock";
 import { getFreshUserRepositoryMock } from "test/mocks/repositories/user.repository.mock";
-import { getFreshRoleServiceMock } from "test/mocks/services/role.service.mock";
 import { getFreshEntityManagerMock } from "test/mocks/utils/entity-manager.mock";
 import { CreateLocalUserDto } from "./dto/requests/create-local-user.dto";
 import { UserRepository } from "./user.repository";
@@ -11,16 +10,11 @@ import { UserService } from "./user.service";
 
 describe("UserService", () => {
   let service: UserService;
-  const roleServiceMock = getFreshRoleServiceMock();
   const userRepositoryMock = getFreshUserRepositoryMock();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UserService,
-        { provide: RoleService, useValue: roleServiceMock },
-        { provide: UserRepository, useValue: userRepositoryMock },
-      ],
+      providers: [UserService, { provide: UserRepository, useValue: userRepositoryMock }],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -31,6 +25,7 @@ describe("UserService", () => {
   });
 
   describe("createLocalUser", () => {
+    const roleMock = getFreshRoleMock();
     const userMock = getFreshUserMock();
     const createLocalUserDto: CreateLocalUserDto = {
       email: "test@example.com",
@@ -42,7 +37,6 @@ describe("UserService", () => {
       userRepositoryMock.findOneBy.mockReset();
       userRepositoryMock.create.mockReset();
       userRepositoryMock.save.mockReset();
-      roleServiceMock.findOrCreateDefaultRole.mockReset();
     });
 
     it("should create a new user when email not exists", async () => {
@@ -50,10 +44,9 @@ describe("UserService", () => {
       userRepositoryMock.create.mockReturnValue(userMock);
       userRepositoryMock.save.mockResolvedValue(userMock);
 
-      const result = await service.createLocalUser(createLocalUserDto);
+      const result = await service.createLocalUser(createLocalUserDto, roleMock);
 
       expect(userRepositoryMock.findOneBy).toHaveBeenCalledWith({ email: createLocalUserDto.email });
-      expect(roleServiceMock.findOrCreateDefaultRole).toHaveBeenCalled();
       expect(userRepositoryMock.create).toHaveBeenCalledWith(createLocalUserDto);
       expect(userRepositoryMock.save).toHaveBeenCalledWith(userMock);
       expect(result).toEqual(userMock);
@@ -64,7 +57,9 @@ describe("UserService", () => {
 
       userRepositoryMock.findOneBy.mockResolvedValue(existingUserMock);
 
-      await expect(service.createLocalUser(createLocalUserDto)).rejects.toThrow(UserEmailAlreadyUsedException);
+      await expect(service.createLocalUser(createLocalUserDto, roleMock)).rejects.toThrow(
+        UserEmailAlreadyUsedException,
+      );
       expect(userRepositoryMock.findOneBy).toHaveBeenCalledWith({ email: createLocalUserDto.email });
     });
 
@@ -76,9 +71,40 @@ describe("UserService", () => {
       userRepositoryMock.create.mockReturnValue(userMock);
       userRepositoryMock.save.mockResolvedValue(userMock);
 
-      const result = await service.createLocalUser(createLocalUserDto, entityManagerMock);
+      const result = await service.createLocalUser(createLocalUserDto, roleMock, entityManagerMock);
 
       expect(result).toEqual(userMock);
+    });
+  });
+
+  describe("markEmailIsVerified", () => {
+    const userMock = getFreshUserMock();
+
+    beforeEach(() => {
+      userRepositoryMock.save.mockReset();
+    });
+
+    it("should save user with isEmailVerified flag set to true and return it", async () => {
+      userRepositoryMock.save.mockResolvedValue(userMock);
+
+      const result = await service.markEmailIsVerified(userMock);
+
+      expect(userRepositoryMock.save).toHaveBeenCalledWith(userMock);
+      expect(result).toBe(userMock);
+      expect(result.isEmailVerified).toBe(true);
+    });
+
+    it("should use entityManager if provided", async () => {
+      const entityManagerMock = getFreshEntityManagerMock();
+
+      entityManagerMock.getRepository.mockReturnValue(userRepositoryMock);
+      userRepositoryMock.save.mockResolvedValue(userMock);
+
+      const result = await service.markEmailIsVerified(userMock);
+
+      expect(userRepositoryMock.save).toHaveBeenCalledWith(userMock);
+      expect(result).toBe(userMock);
+      expect(result.isEmailVerified).toBe(true);
     });
   });
 });
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,29 +2,31 @@ import { EntityManager } from "typeorm";
 import { Injectable } from "@nestjs/common";
 import { BaseService } from "src/shared/base/base.service";
 import { UserEmailAlreadyUsedException } from "src/shared/exceptions/user/user-email-already-used.exception";
-import { RoleService } from "src/modules/role/role.service";
+import { Role } from "src/modules/role/entities/role.entity";
 import { User } from "./entities/user.entity";
 import { CreateLocalUserDto } from "./dto/requests/create-local-user.dto";
 import { UserRepository } from "./user.repository";
 
 @Injectable()
 export class UserService extends BaseService {
-  constructor(
-    private readonly roleService: RoleService,
-    private readonly userRepository: UserRepository,
-  ) {
+  constructor(private readonly userRepository: UserRepository) {
     super();
   }
 
-  async createLocalUser(createLocalUserDto: CreateLocalUserDto, entityManager?: EntityManager) {
+  async createLocalUser(createLocalUserDto: CreateLocalUserDto, role: Role, entityManager?: EntityManager) {
     const repository = this.getRepository(User, this.userRepository, entityManager);
     const existingUser = await repository.findOneBy({ email: createLocalUserDto.email });
     if (existingUser) throw new UserEmailAlreadyUsedException();
 
-    const role = await this.roleService.findOrCreateDefaultRole();
     const newUser = repository.create(createLocalUserDto);
     newUser.role = role;
 
     return await repository.save(newUser);
+  }
+
+  markEmailIsVerified(user: User, entityManager?: EntityManager) {
+    const repository = this.getRepository(User, this.userRepository, entityManager);
+    user.isEmailVerified = true;
+    return repository.save(user);
   }
 }
